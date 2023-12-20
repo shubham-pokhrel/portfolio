d22db2c87be6e2bc5713542cf1b3a006f6ff84b2
Shubham Pokhrel <shubhampokhrel234@gmail.com>
2023-12-20 18:40:19 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my hardware and cloud experience I continually look for new and better ways to make tech accessible by all.";
+  "I'm passionate about solving problems in new creative ways to drive innovation. By leveraging my hardware and cloud experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
